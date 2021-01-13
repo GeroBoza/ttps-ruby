e34fae3c58230dc1d6b1be0ced245aa778a5afe8
Geronimo Boza <geronimoboza@gmail.com>
2021-01-13 15:37:05 -0800

    rutas corregidas, faltan algunas de notes

--- proyecto-integrador/app/controllers/notes_controller.rb
+++ proyecto-integrador/app/controllers/notes_controller.rb
@@ -1,20 +1,20 @@
 class NotesController < ApplicationController
+  before_action :set_user
+  before_action :set_book
   before_action :set_note, only: [:show, :edit, :update, :destroy]
 
   # GET /notes
-  # GET /notes.json
   def index
-    @notes = Note.all
+    @notes = @book.notes
   end
 
   # GET /notes/1
-  # GET /notes/1.json
   def show
   end
 
   # GET /notes/new
   def new
-    @note = Note.new
+    @note = @book.notes.new
   end
 
   # GET /notes/1/edit
@@ -22,53 +22,47 @@ class NotesController < ApplicationController
   end
 
   # POST /notes
-  # POST /notes.json
   def create
-    @note = Note.new(note_params)
+    @note = @book.notes.new(note_params)
 
-    respond_to do |format|
-      if @note.save
-        format.html { redirect_to @note, notice: 'Note was successfully created.' }
-        format.json { render :show, status: :created, location: @note }
-      else
-        format.html { render :new }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
-      end
+    if @note.save
+      redirect_to [@user,@book,@note], notice: 'Note was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /notes/1
-  # PATCH/PUT /notes/1.json
   def update
-    respond_to do |format|
-      if @note.update(note_params)
-        format.html { redirect_to @note, notice: 'Note was successfully updated.' }
-        format.json { render :show, status: :ok, location: @note }
-      else
-        format.html { render :edit }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
-      end
+    if @note.update(note_params)
+      redirect_to [@user,@book,@note], notice: 'Note was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /notes/1
-  # DELETE /notes/1.json
   def destroy
     @note.destroy
-    respond_to do |format|
-      format.html { redirect_to notes_url, notice: 'Note was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to user_book_notes_url, notice: 'Note was successfully destroyed.'
   end
 
   private
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+
+    def set_book
+      @book = Book.find(params[:book_id])
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_note
-      @note = Note.find(params[:id])
+      @note = @book.notes.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def note_params
-      params.fetch(:note, {})
+      params.require(:note).permit(:book_id, :title, :content)
     end
 end
